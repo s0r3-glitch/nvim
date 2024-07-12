35467fd8ba7251b110f31a78e608d86246fd2ff9
s0r3-glitch <ggoodson2432@gmail.com>
2024-07-12 13:55:25 -0700

    changed shit so it uses the plugin for my config

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-local function open_nvim_tree()
-  -- open the tree
-  require("nvim-tree.api").tree.open()
-end
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -13,15 +9,10 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.keymap.set("", "<up>", "<nop>", { noremap = true })
-vim.keymap.set("", "<down>", "<nop>", { noremap = true })
-vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
-vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
-vim.opt.mousescroll = "ver:0,hor:0" 
-vim.opt.mouse = ""
 
 vim.opt.rtp:prepend(lazypath)
-require("vim-options")
-require("lazy").setup("plugins")
---vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-require'lspconfig'.pyright.setup{}
+require("lazy").setup({
+	spec = {
+	{"s0r3-glitch/s0r3fig", import="s0r3fig.lua.plugins"}
+},
+})
